575416aba08654ebc1488dae837eee80be0f7737
CheeseSnax <jonah.d.stringfellow@gmail.com>
2026-09-09 12:34:00 -0600

    added build.sh

---
+++ build/CMakeFiles/Slitherer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Slitherer.dir/link.d"
+  "CMakeFiles/Slitherer.dir/src/main.cpp.o"
+  "CMakeFiles/Slitherer.dir/src/main.cpp.o.d"
+  "Slitherer"
+  "Slitherer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Slitherer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Slitherer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Slitherer.
+# This may be replaced when dependencies are built.
